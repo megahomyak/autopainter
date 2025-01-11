afb2081923a5092abf5204ecb3edf71ac37fd22f
megahomyak <g.megahomyak@gmail.com>
2025-01-11 19:08:16 +0500

    Tried to use the "layer 1" button as a temporary location, but it seems too risky, I should go for "Reset Zoom"

--- script.py
+++ script.py
@@ -9,8 +9,10 @@ from collections import namedtuple
 Pixel = namedtuple("Pixel", "r g b")
 Spot = namedtuple("Spot", "x y")
 Part = namedtuple("Part", "x y")
+Size = namedtuple("Size", "width height")
 Bias = namedtuple("Bias", "x y")
-Bounds = namedtuple("Bias", "bottom top left right")
+Area = namedtuple("Bias", "bottom top left right")
+ScreenParams = namedtuple("ScreenParams", "canvas_area color_text_box_spot current_color_spot color_confirmation_spot")
 
 # I/O adapters below
 
@@ -21,16 +23,33 @@ def get_screen_pixel(spot):
     blue = (pixel >> 16) & 0xFF
     return Pixel(red, green, blue)
 
-def get_screen_width():
-    return ImageGrab.grab().width
+def get_screen_area_pixels(area):
+    return map(
+        lambda pixel: Pixel(*pixel),
+        ImageGrab.grab(bbox=(area.left, area.top, area.right, area.bottom)).getdata(),
+    )
 
-def get_screen_height():
-    return ImageGrab.grab().height
+def make_screenshot():
+    return ImageGrab.grab()
+
+def get_screenshot_pixel(screenshot, spot):
+    channels = screenshot.getpixel((spot.x, spot.y))
+    return Pixel(*channels)
+
+def get_screenshot_width(screenshot):
+    return screenshot.width
+
+def get_screenshot_height(screenshot):
+    return screenshot.height
 
 def click_mouse(spot):
+    autoit.mouse_move(spot.x, spot.y, speed=0)
+    autoit.mouse_move(spot.x - 1, spot.y, speed=0)
     autoit.mouse_click("left", spot.x, spot.y, speed=0)
 
 def move_mouse(spot):
+    autoit.mouse_move(spot.x, spot.y, speed=0)
+    autoit.mouse_move(spot.x - 1, spot.y, speed=0)
     autoit.mouse_move(spot.x, spot.y, speed=0)
 
 def send_ctrl_a_color_in_hex_and_enter(color):
@@ -46,21 +65,56 @@ def load_quantized_image_pixels(colors_amount, file_path, target_side_size):
 
 # I/O adapters above
 
-def find_bound(center, bias):
+def find_bound(screenshot, center, bias):
     current = center
     while True:
         new_spot = Spot(x=current.x + bias.x, y=current.y + bias.y)
-        if get_screen_pixel(new_spot) != Pixel(r=255, g=255, b=255):
+        if get_screenshot_pixel(screenshot, new_spot) != Pixel(r=255, g=255, b=255):
             return current
         current = new_spot
 
-def get_screen_canvas_bounds():
-    center = Spot(x=get_screen_width()//2, y=get_screen_height()//2)
-    return Bounds(
-        bottom=find_bound(center, Bias(x=0, y=1)).y,
-        top=find_bound(center, Bias(x=0, y=-1)).y,
-        left=find_bound(center, Bias(x=-1, y=0)).x,
-        right=find_bound(center, Bias(x=1, y=0)).x,
+def get_color_picker_spot(picker_upper_left, picker_size, part):
+    return Spot(
+        x=int(part.x*picker_size.width) + picker_upper_left.x,
+        y=int(part.y*picker_size.height) + picker_upper_left.y,
+    )
+
+def get_screen_params():
+    screenshot = make_screenshot()
+    width = get_screenshot_width(screenshot)
+    height = get_screenshot_height(screenshot)
+    center = Spot(x=width//2, y=height//2)
+    # Below constants were taken on my PC. Imprecise. Looking for better constants only resolved in conflicts
+    # Not the same constant, but a good demonstration of a conflict:
+    # 1080*0.525004074074074 = 567.0043999999999, 566 required => too much
+    # 900*0.525004074074074 = 472.5036666666666, 473 required => too little
+    picker_w_mult = 475/1920
+    picker_h_mult = 473/1080
+    # Constants end
+    picker_size = Size(
+        width=picker_w_mult * width,
+        height=picker_h_mult * height,
+    )
+    picker_upper_left = Spot(
+        x=int(width*0.40),
+        y=int(height*0.30),
+    )
+    return ScreenParams(
+        canvas_area=Area(
+            bottom=find_bound(screenshot, center, Bias(x=0, y=1)).y,
+            top=find_bound(screenshot, center, Bias(x=0, y=-1)).y,
+            left=find_bound(screenshot, center, Bias(x=-1, y=0)).x,
+            right=find_bound(screenshot, center, Bias(x=1, y=0)).x,
+        ),
+        color_text_box_spot=get_color_picker_spot(
+            picker_upper_left, picker_size, Part(x=0.65, y=0.77),
+        ),
+        current_color_spot=get_color_picker_spot(
+            picker_upper_left, picker_size, Part(x=1/17, y=0.77),
+        ),
+        color_confirmation_spot=get_color_picker_spot(
+            picker_upper_left, picker_size, Part(x=0.22, y=0.77),
+        ),
     )
 
 def load_any_image_pixels(colors_amount, directory, target_side_size):
@@ -85,7 +139,13 @@ def sort_image_pixels(image_pixels, image_side_size):
             saved_pixels.setdefault(current_color, []).append(spot)
     return saved_pixels
 
-def transform_part_to_gui_spot(bottom_left, side, part):
+def check_color_in_area(area, color):
+    for pixel in get_screen_area_pixels(area):
+        if pixel == color:
+            return True
+    return False
+
+def transform_part_to_button_spot(bottom_left, side, part):
     return Spot(x=bottom_left.x + int(part.x*side), y=bottom_left.y + int(part.y*side))
 
 class ChangeWaiter:
@@ -93,235 +153,64 @@ class ChangeWaiter:
         self.spot = spot
         self.old_pixel = old_pixel
     @classmethod
-    def record(cls, bottom_left, side, bias):
-        spot = transform_bias_to_gui_spot(bias)
-        old_pixel = get_screen_pixel(self.spot)
+    def record(cls, spot):
+        old_pixel = get_screen_pixel(spot)
         return cls(spot, old_pixel)
     def wait(self):
         while True:
             new_pixel = get_screen_pixel(spot=self.spot)
             if new_pixel != self.old_pixel:
-                self.old_pixel = new_pixel
-                break
-    def clone(self):
-        return ChangeWaiter(self.spot, self.old_pixel)
+                return ChangeWaiter(self.spot, new_pixel)
 
-def draw_image(sorted_image_pixels, screen_canvas_bounds, canvas_side_pixels):
-    side = min(screen_canvas_bounds.bottom - screen_canvas_bounds.top, screen_canvas_bounds.right - screen_canvas_bounds.left)
-    bottom_left = Spot(x=screen_canvas_bounds.left, y=screen_canvas_bounds.bottom)
-    click_mouse(spot=transform_part_to_gui_spot(bottom_left, side, Part(x=0.2, y=0.1)))
-    time.sleep(2)
-    move_mouse(spot=transform_part_to_gui_spot(bottom_left, side, Part(x=0.25, y=-0.13)))
+def change_color(pixel):
+    # 107 161 249: "selection blue"
+    pass
+
+def draw_image(sorted_image_pixels, screen_params, canvas_side_resolution):
+    side = min(
+        screen_params.canvas_area.bottom - screen_params.canvas_area.top,
+        screen_params.canvas_area.right - screen_params.canvas_area.left,
+    )
+    COLOR_PICKER_BUTTON_PART = Part(x=0.15, y=0.1)
+    LAYER_1_HIDE_BUTTON_PART = Part(x=1.43, y=-0.55)
+    bottom_left = Spot(
+        x=screen_params.canvas_area.left,
+        y=screen_params.canvas_area.bottom,
+    )
+    color_picker_button_spot = transform_part_to_button_spot(bottom_left, side, COLOR_PICKER_BUTTON_PART)
+    layer_1_hide_button_spot = transform_part_to_button_spot(bottom_left, side, LAYER_1_HIDE_BUTTON_PART)
+    move_mouse(layer_1_hide_button_spot)
+    return
+    move_mouse(screen_params.color_text_box_spot)
+    time.sleep(1)
+    move_mouse(screen_params.current_color_spot)
+    time.sleep(1)
+    move_mouse(screen_params.color_confirmation_spot)
     return
     for pixel, spots in sorted_image_pixels.items():
-        change_color(pixel.color)
+        change_color(pixel)
         for spot in spots:
             screen_pixel_spot = Spot(
-                x=side / canvas_side_pixels * pixel.spot.x,
-                y=side / canvas_side_pixels * pixel.spot.y,
+                x=side / canvas_side_resolution * pixel.spot.x,
+                y=side / canvas_side_resolution * pixel.spot.y,
             )
             click(screen_pixel_spot)
 
 def main():
-    CANVAS_SIDE_PIXELS = 338
+    CANVAS_SIDE_RESOLUTION = 200
     COLORS_AMOUNT = 255
 
-    input_image_pixels = load_any_image_pixels(COLORS_AMOUNT, directory=".", target_side_size=CANVAS_SIDE_PIXELS)
+    input_image_pixels = load_any_image_pixels(COLORS_AMOUNT, directory=".", target_side_size=CANVAS_SIDE_RESOLUTION)
     if input_image_pixels is None:
         print("Please, provide an input image.")
         exit(1)
 
-    compressed_input_image_pixels = sort_image_pixels(input_image_pixels, image_side_size=CANVAS_SIDE_PIXELS)
+    compressed_input_image_pixels = sort_image_pixels(input_image_pixels, image_side_size=CANVAS_SIDE_RESOLUTION)
 
     time.sleep(3) # Wait for the user to open Roblox
 
-    screen_canvas_bounds = get_screen_canvas_bounds()
+    screen_params = get_screen_params()
 
-    draw_image(compressed_input_image_pixels, screen_canvas_bounds, CANVAS_SIDE_PIXELS)
+    draw_image(compressed_input_image_pixels, screen_params, CANVAS_SIDE_RESOLUTION)
 
 main()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# OLD CODE BELOW
-
-# Image interactor adapters:
-
-def make_screenshot(area=None):
-    move(SN(x=0, y=0))
-    if area is not None:
-        area = (area.left, area.top, area.right, area.bottom)
-    return ImageGrab.grab(bbox=area)
-
-def get_pixel(image, spot):
-    r,g,b = image.getpixel((spot.x, spot.y))
-    return SN(r=r, g=g, b=b)
-
-def get_width(image):
-    return image.width
-
-def get_height(image):
-    return image.height
-
-def get_input_image(directory):
-    image = None
-    for file_name in os.listdir(directory):
-        try:
-            image = Image.open(file_name)
-        except ValueError:
-            pass
-        else:
-            break
-    return image
-
-def resize(image, new_size):
-    return image.resize((new_size.width, new_size.height))
-
-# Image interactor adapters end
-
-# Gui interactor adapters:
-
-def click(spot):
-    autoit.mouse_click("left", spot.x, spot.y, speed=2)
-
-def move(spot):
-    autoit.mouse_move(spot.x, spot.y, speed=0)
-
-def enter_text(text):
-    autoit.send(text.replace("#", "{#}"))
-    time.sleep(0.5)
-
-def hold_ctrl_and_enter_text(text):
-    autoit.send("^" + text.replace("#", "{#}"))
-    time.sleep(0.5)
-
-# Gui interactor adapters end
-
-class BoundFinder:
-    def __init__(self, screenshot):
-        self.center = SN(x=get_width(screenshot)//2, y=get_height(screenshot)//2)
-        self.screenshot = screenshot
-    def find_bound(self, bias):
-        current = self.center
-        while True:
-            new = SN(x=current.x + bias.x, y=current.y + bias.y)
-            if get_pixel(image=self.screenshot, spot=new) != SN(r=255, g=255, b=255):
-                return current
-            current = new
-
-def paint(spot):
-    click(spot)
-    time.sleep(0.01)
-
-def iter_pixels(image):
-    for x in range(get_width(image)):
-        for y in range(get_height(image)):
-            spot = SN(x=x, y=y)
-            pixel = get_pixel(image, spot)
-            yield (spot, pixel)
-
-class PixelSizeMeasurer:
-    def __init__(self, center, canvas_area, pick_color):
-        self.canvas_area = canvas_area
-        self.center = center
-        self.pick_color = pick_color
-    def measure(self):
-        self.pick_color(SN(r=255, g=0, b=0))
-        paint(self.center)
-        time.sleep(0.5) # Painting still takes some time
-        screenshot = make_screenshot(area=self.canvas_area)
-        top = float("+inf")
-        bottom = float("-inf")
-        left = float("+inf")
-        right = float("-inf")
-        for spot, pixel in iter_pixels(screenshot):
-            if pixel == SN(r=255, g=0, b=0):
-                if spot.x < left:
-                    left = spot.x
-                elif spot.x > right:
-                    right = spot.x
-                if spot.y > bottom:
-                    bottom = spot.y
-                elif spot.y < top:
-                    top = spot.y
-        return min(bottom - top, right - left)
-
-class GuiInteractors:
-    def __init__(self, bottom_left, side):
-        self.side = side
-        self.bottom_left = bottom_left
-
-    def click_gui(self, bias):
-        click(spot=SN(x=self.bottom_left.x + int(bias.x*self.side), y=self.bottom_left.y + int(bias.y*self.side)))
-        time.sleep(0.5)
-
-    def pick_color(self, color):
-        color_hex = "#%02x%02x%02x" % (color.r, color.g, color.b)
-
-        self.click_gui(bias=SN(x=0.2, y=0.1)) # Open the color picker
-        self.click_gui(bias=SN(x=0.7, y=-0.13)) # Switch to color field
-        hold_ctrl_and_enter_text("a") # Select the current color
-        enter_text(color_hex) # Write the new color
-        self.click_gui(bias=SN(x=0.38, y=-0.13)) # Confirm color
-
-def main():
-    input_image = get_input_image(".")
-    if input_image is None:
-        print("Please, provide an input image.")
-        exit(1)
-
-    time.sleep(3) # Wait for the user to open Roblox
-
-    bound_finder = BoundFinder(screenshot=make_screenshot())
-
-    bottom = bound_finder.find_bound(bias=SN(x=0, y=1)).y
-    top = bound_finder.find_bound(bias=SN(x=0, y=-1)).y
-    left = bound_finder.find_bound(bias=SN(x=-1, y=0)).x
-    right = bound_finder.find_bound(bias=SN(x=1, y=0)).x
-    
-    side = bottom - top
-    bottom_left = SN(x=left, y=bottom)
-    
-    gui_interactors = GuiInteractors(bottom_left, side)
-
-    pixel_size = PixelSizeMeasurer(center=bound_finder.center, canvas_area=SN(bottom=bottom, top=top, left=left, right=right), pick_color=gui_interactors.pick_color).measure()
-    side_pixels = side // pixel_size
-    print(side_pixels)
-    return
-
-    input_image = resize(input_image, SN(width=side_pixels, height=side_pixels))
-    colors_to_spots = {}
-    for spot, pixel in iter_pixels(input_image):
-        if pixel == SN(r=255, g=255, b=255): continue
-        canvas_spot = SN(x=left + spot.x*pixel_size+1, y=top + spot.y*pixel_size+1) # IDK why +1
-        colors_to_spots.setdefault((pixel.r, pixel.g, pixel.b), []).append(canvas_spot)
-
-    for color, spots in colors_to_spots.items():
-        color = SN(r=color[0], g=color[1], b=color[2])
-        gui_interactors.pick_color(color)
-        for spot in spots:
-            if keyboard.is_pressed("q"):
-                exit(0)
-            click(spot)
